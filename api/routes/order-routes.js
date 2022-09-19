837bda9492e333b55525ad2ce8112f6e07d22f9a
diogolosa77 <diogolosa@gmail.com>
2022-09-19 20:13:45 +0100

    Fixed order routes

--- api/routes/order-routes.js
+++ api/routes/order-routes.js
@@ -10,50 +10,37 @@ const router = Router()
 router.post('/new', async (req, res) => {   
     const token = req.headers['x-access-token']
     const user = jwt.verify(token, process.env.JWT_SECRET)
-    const doc = await User.findOne({email: user.email})
+    let doc 
+    if(user){
+        doc = await User.findOne({email: user.email})
+    }else{
+        return res.status(503).send('Access denied')
+    }
     
     if(doc.password === user.pass){
-        
         try {
-            if(req.body.paymentResult){
-                const order = new Order(
-                    {
-                        createdBy: doc._id,
-                        event: req.body.event,
-                        ticketQty: req.body.ticketQty,
-                        totalAmount: req.body.totalAmount,
-                        paymentResult: req.body.paymentResult,
-                        isPaid: true
-                    }
-                )
-            }else{
-                const order = new Order(
-                    {
-                        createdBy: doc._id,
-                        event: req.body.event,
-                        ticketQty: req.body.ticketQty,
-                        totalAmount: req.body.totalAmount,
-                        isPaid: true
-                    }
-                )
-            }
-            
-            const orderComplete = await order.save()
-            if(orderComplete){
-                try {
-                    let event = await Event.findById(req.body.event)
-                    let sold = event.ticketsSold + req.body.ticketQty
-                    let available = event.ticketsAvailable - req.body.ticketQty
-                    if(available === 0){
-                        event = await Event.findByIdAndUpdate(req.body.event, {ticketsSold: sold, ticketsAvailable: available, status: 'sold out'})
-                    }
-                    event = await Event.findByIdAndUpdate(req.body.event, {ticketsSold: sold, ticketsAvailable: available})
-
-                    // EM FALTA:::criar bilhetes enviar emails
-                    return res.status(201).json({success: true}) 
-                } catch (error) {
-                    return res.status(400).json({success: false, error: error})
+            const order = new Order(
+                {
+                    createdBy: doc._id,
+                    event: req.body.event,
+                    ticketQty: req.body.ticketQty,
+                    totalAmount: req.body.totalAmount,
+                    isPaid: true
                 }
+            )
+            
+        const orderComplete = await order.save()
+            if(orderComplete){
+            let event = await Event.findById(req.body.event)
+            let sold = event.ticketsSold + req.body.ticketQty
+            let available = event.ticketsAvailable - req.body.ticketQty
+            if(available === 0){
+                event = await Event.findByIdAndUpdate(req.body.event, {ticketsSold: sold, ticketsAvailable: available, status: 'sold out'})
+            }
+            event = await Event.findByIdAndUpdate(req.body.event, {ticketsSold: sold, ticketsAvailable: available})
+
+            // EM FALTA:::criar bilhetes enviar emails
+            return res.status(201).json({success: true}) 
             }
         } catch (error) {
             return res.status(400).json({success: false, error: error})
